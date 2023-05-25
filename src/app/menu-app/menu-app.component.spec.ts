49d0bd8fcc7b92d22ccd361a34646a4508f2c407
Mateus Vitor <mateusvitor01.mv@gmail.com>
2023-05-25 16:38:04 -0300

    projeto criado

---
+++ src/app/menu-app/menu-app.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MenuAppComponent } from './menu-app.component';
+
+describe('MenuAppComponent', () => {
+  let component: MenuAppComponent;
+  let fixture: ComponentFixture<MenuAppComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [MenuAppComponent]
+    });
+    fixture = TestBed.createComponent(MenuAppComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
